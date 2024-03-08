9d78d64f35f1a2b294daa1c7582a038046182a6f
Nayan <nayanunni95@gmail.com>
2024-03-08 19:03:15 +0530

    add responsiveness and dynamic div layout

--- src/components/ToDoMain.jsx
+++ src/components/ToDoMain.jsx
@@ -1,39 +1,27 @@
-import "./ToDoMain.css";
+import './ToDoMain.css';
 
 function ToDoMain() {
   return (
     <>
-      <div className="container flex flex-align flex-content">
-        <div className="wrapper flex flex-align flex-content flex-wrap">
-          <div className="header flex flex-align flex-content flex-dir">
+      <div className='container flex flex-align flex-content'>
+        <div className='wrapper flex flex-align flex-content flex-wrap'>
+          <div className='header flex flex-align flex-content flex-dir'>
             <h1>React ToDo List...</h1>
-            <div className="input-div flex">
-              <input type="text" placeholder="Enter description" />
+            <div className='input-div flex'>
+              <input type='text' placeholder='Enter description' />
               <button>
-                <i className="fa-solid fa-plus fa-xl"></i>
+                <i className='fa-solid fa-plus fa-xl'></i>
               </button>
             </div>
           </div>
-          <div className="footer">
-            <div className="content flex flex-align">
-              <input type="checkbox" />
-              <h4>Second year semester exam</h4>
+          <div className='footer'>
+            <div className='content flex flex-align'>
+              <input type='checkbox' />
+              <div className='content-data flex flex-align flex-content'>
+                <h4>Second year semester exam</h4>
+              </div>
               <button>
-                <i className="fa-solid fa-trash fa-xl"></i>
-              </button>
-            </div>
-            <div className="content flex flex-align">
-              <input type="checkbox" />
-              <h4>Second year semester exam</h4>
-              <button>
-                <i className="fa-solid fa-trash fa-xl"></i>
-              </button>
-            </div>
-            <div className="content flex flex-align">
-              <input type="checkbox" />
-              <h4>Second year semester exam</h4>
-              <button>
-                <i className="fa-solid fa-trash fa-xl"></i>
+                <i className='fa-solid fa-trash fa-xl'></i>
               </button>
             </div>
           </div>
