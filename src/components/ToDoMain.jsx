4a426042696a64a89b19c589d06d4bab7d864613
Nayan <nayanunni95@gmail.com>
2024-03-11 20:23:51 +0530

    add create, checked and delete actions

--- src/components/ToDoMain.jsx
+++ src/components/ToDoMain.jsx
@@ -1,6 +1,9 @@
 import './ToDoMain.css';
+import { useState } from 'react';
 
 function ToDoMain() {
+  const [toDo, setToDo] = useState([]);
+  const [data, setData] = useState('');
   return (
     <>
       <div className='container flex flex-align flex-content'>
@@ -8,22 +11,59 @@ function ToDoMain() {
           <div className='header flex flex-align flex-content flex-dir'>
             <h1>React ToDo List...</h1>
             <div className='input-div flex'>
-              <input type='text' placeholder='Enter description' />
-              <button>
+              <input
+                type='text'
+                onChange={(e) => {
+                  setData(e.target.value);
+                }}
+                placeholder='Enter description'
+              />
+              <button
+                onClick={() => {
+                  setToDo([
+                    ...toDo,
+                    { des: data, id: Date.now(), check: false },
+                  ]);
+                }}>
                 <i className='fa-solid fa-plus fa-xl'></i>
               </button>
             </div>
           </div>
           <div className='footer'>
-            <div className='content flex flex-align'>
-              <input type='checkbox' />
-              <div className='content-data flex flex-align flex-content'>
-                <h4>Second year semester exam</h4>
-              </div>
-              <button>
-                <i className='fa-solid fa-trash fa-xl'></i>
-              </button>
-            </div>
+            {toDo.map((obj, index) => {
+              return (
+                <div key={index} className='content flex flex-align'>
+                  <input
+                    onClick={(e) => {
+                      setToDo(
+                        toDo.filter((index) => {
+                          if (obj.id === index.id) {
+                            index.status = e.target.checked;
+                          }
+                          return index;
+                        })
+                      );
+                    }}
+                    type='checkbox'
+                  />
+                  <div className='content-data flex flex-align flex-content'>
+                    <h4>{obj.des}</h4>
+                  </div>
+                  <button
+                    onClick={() => {
+                      setToDo(
+                        toDo.filter((index) => {
+                          if (obj.id != index.id) {
+                            return index;
+                          }
+                        })
+                      );
+                    }}>
+                    <i className='fa-solid fa-trash fa-xl'></i>
+                  </button>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
